6b9a92881be08b1c50c00e8d9ce503e7b47c6d23
Vraj Patel <v.alphatron1999@gmail.com>
2024-10-25 13:14:38 +0200

    clean main

--- descriptorModelLevelWind/docker/run-pipeline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-REPO_DIR="$HOME/hackathon/descriptorModelLevelWind"
-AWS_CRED_DIR="$HOME/.aws/"
-docker run \
-   -v $REPO_DIR:/repo \
-   -v $AWS_CRED_DIR:$HOME/.aws/\
-   -p 7860:7860\
-   winddesk:v1\
-   /bin/bash -c "source ~/miniconda3/bin/activate && conda activate runenv && cd /repo && python pipeline.py run descriptor.json"
